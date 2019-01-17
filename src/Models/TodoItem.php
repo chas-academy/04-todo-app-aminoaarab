53e689783d5e4a77df8e8ae80841d1039b1334de
Amin Aarab <amin.aarab@chasacademy.se>
2019-01-17 15:35:22 +0100

    Added the UPDATE sql query.

--- src/Models/TodoItem.php
+++ src/Models/TodoItem.php
@@ -19,7 +19,14 @@ class TodoItem extends Model
 
     public static function updateTodo($todoId, $title, $completed = null)
     {
-        
+        $query = "UPDATE todos 
+        SET title = '$title', completed = '$completed' 
+        WHERE id = '$todoId'";
+
+        self::$db->query($query);
+        $result = self::$db->execute();
+
+        return $result;
     }
 
     public static function deleteTodo($todoId)
