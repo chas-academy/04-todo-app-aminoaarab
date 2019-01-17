60f140a73e61c78b09b5dbcb0f91913634b26f80
Amin Aarab <amin.aarab@chasacademy.se>
2019-01-17 17:10:39 +0100

    Removed comments.

--- src/Controllers/TodoController.php
+++ src/Controllers/TodoController.php
@@ -5,7 +5,7 @@ use Todo\Database;
 use Todo\TodoItem;
 
 class TodoController extends Controller {
-    //AMINAMINAMINAMINAMIN
+    
     public function get()
     {
         $todos = TodoItem::findAll();
@@ -24,12 +24,11 @@ class TodoController extends Controller {
 
     public function update($urlParams)
     {
-        $body = filter_body(); // gives you the body of the request (the "envelope" contents)
-        $todoId = $urlParams['id']; // the id of the todo we're trying to update
-        $completed = isset($body['status']) ? 'true' : 'false'; // whether or not the todo has been checked or not
+        $body = filter_body();
+        $todoId = $urlParams['id'];
+        $completed = isset($body['status']) ? 'true' : 'false';
 
         $result = TodoItem::updateTodo($todoId, $body['title'], $completed);
-        
         if ($result) {
           $this->redirect('/');
         }
@@ -38,29 +37,10 @@ class TodoController extends Controller {
 
     public function delete($urlParams)
     {
-      $body = filter_body();
       $result = TodoItem::deleteTodo($urlParams['id']);
       
       if ($result) {
         $this->redirect('/');
       }
     }
-
-    /**
-     * OPTIONAL Bonus round!
-     * 
-     * The two methods below are optional, feel free to try and complete them
-     * if you're aiming for a higher grade.
-     */
-    // public function toggle()
-    // {
-    //   // (OPTIONAL) TODO: This action should toggle all todos to completed, or not completed.
-    // }
-
-    // public function clear($completed)
-    // {
-    //   // (OPTIONAL) TODO: This action should remove all completed todos from the table.
-    //   }
-    //}
-
 }
--- src/Models/TodoItem.php
+++ src/Models/TodoItem.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace Todo;
-//AMINAMINAMINAMIN
 class TodoItem extends Model
 {
     const TABLENAME = 'todos'; // This is used by the abstract model, don't touch
@@ -39,27 +38,4 @@ class TodoItem extends Model
 
         return $result;
     }
-    
-    //(Optional bonus methods below)
-    // public static function toggleTodos($completed)
-    // {
-    //     // TODO: Implement me!
-    //     // This is to toggle all todos either as completed or not completed
-
-    // }
-
-    // public static function clearCompletedTodos($todoId)
-    // {
-    //     // TODO: Implement me!
-    //     // This is to delete all the completed todos from the database
-
-    //     $query = "DELETE FROM todos 
-    //     WHERE id = '$todoId'";
-
-    //     self::$db->query($query);
-    //     $result = self::$db->execute();
-
-    //     return $result; 
-    // }
-
 }
